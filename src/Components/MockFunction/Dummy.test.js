0d38c29758696d80d4e6db85bde70ecdf470cf72
Aravindh Karuvelraj <aravindh.karuvelraj@zucisystems.com>
2023-04-04 21:07:23 +0530

    Mock multiple return values in same function

--- src/Components/MockFunction/Dummy.test.js
+++ src/Components/MockFunction/Dummy.test.js
@@ -1,8 +1,9 @@
-import { render, screen,fireEvent} from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 // import { MockFunction } from './MockFunction';
 import Dummy from "./Dummy";
+import { MockFunction } from './MockFunction';
 
-// jest.mock('./MockFunction')  
+jest.mock('./MockFunction')  
 
 test('Component is Rendered',()=>{
     render(<Dummy/>);
@@ -19,6 +20,29 @@ test("Click Login Button",()=>{
 
     expect(mockCallback.mock.results[0].value).toBe('Hai');
     expect(mockCallback.mock.results[1].value).toBe('Hello');
+
+})
+
+
+test("mock multiple calls",()=>{
+MockFunction.mockReturnValueOnce("First call")
+.mockReturnValueOnce("Second call")
+.mockReturnValue("Default call")
+
+MockFunction()
+
+expect(MockFunction.mock.results[0].value).toBe("First call")
+MockFunction()
+MockFunction()
+MockFunction()
+MockFunction()
+expect(MockFunction.mock.results[1].value).toBe("Second call")
+expect(MockFunction.mock.results[2].value).toBe("Default call")
+expect(MockFunction.mock.results[3].value).toBe("Default call")
+expect(MockFunction.mock.results[4].value).toBe("Default call")
+
+
+
 
 })
 
